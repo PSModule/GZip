5c860ab9d5672e5bc88afae2c764f6eb673c2f02
Marius Storhaug <marstor@hotmail.com>
2025-02-18 01:07:48 +0100

    🩹 [Patch]: Update linting and git configuration and license year (#4)
    ## Description
    
    This pull request includes several updates to configuration files, the
    addition of new PowerShell functions, and minor changes to existing
    files.
    
    Configuration updates:
    
    *
    [`.github/linters/.jscpd.json`](diffhunk://#diff-557094e283c00b23265c1c75872f41c6b1a524a00f0d99dd68ebd22cb63bfdd6R1-R10):
    Added a new JSON configuration file for the JSCPD linter with specific
    settings and ignore patterns.
    *
    [`.github/linters/.powershell-psscriptanalyzer.psd1`](diffhunk://#diff-aae69c9d6774628ed181eacf53aee0f38eb6c2f53492cf3a5b7f7bdb6ef43b6aL1-L16):
    Updated the PowerShell Script Analyzer configuration to enable specific
    rules and set custom rule paths, severities, and indentation settings.
    
    New PowerShell functions:
    
    *
    [`dev/gzip.ps1`](diffhunk://#diff-ccebdb4d1513546a108e54665c3dd927284620c7b6f2a63092ecaa4b014809b4R1-R116):
    Introduced `Expand-GZipArchive` and `Compress-GZipArchive` functions to
    handle GZip compression and decompression with detailed documentation
    and examples.
    
    Workflow and license updates:
    
    *
    [`.github/workflows/Linter.yml`](diffhunk://#diff-482e65806ed9e4a7320f14964764086b91fed4a28d12e4efde1776472e147e79R30):
    Disabled JSON Prettier validation in the linter workflow.
    *
    [`LICENSE`](diffhunk://#diff-c693279643b8cd5d248172d9c22cb7cf4ed163a3c98c8a3f69c2717edd3eacb7L3-R3):
    Updated the copyright year from 2024 to 2025.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ dev/gzip.ps1
@@ -0,0 +1,116 @@
+﻿function Expand-GZipArchive {
+    <#
+        .SYNOPSIS
+        Expands a GZip-compressed file to its original form.
+
+        .DESCRIPTION
+        The Expand-GZipArchive function decompresses a GZip-compressed file and saves the output
+        to a specified location. If no destination path is provided, the function removes the `.gz`
+        extension from the input file path and saves the decompressed file there.
+
+        .EXAMPLE
+        Expand-GZipArchive -Path "C:\Temp\archive.gz"
+
+        Output:
+        ```powershell
+        The file 'C:\Temp\archive.gz' is successfully decompressed to 'C:\Temp\archive'.
+        ```
+
+        Decompresses the GZip file 'C:\Temp\archive.gz' and saves it as 'C:\Temp\archive'.
+
+        .OUTPUTS
+        System.String. Returns the destination file path after successful extraction.
+
+        .LINK
+        https://psmodule.io/Expand/Functions/Expand-GZipArchive
+    #>
+    [CmdletBinding()]
+    param(
+        # Path to the GZip archive.
+        [Parameter(Mandatory)]
+        [string] $Path,
+
+        # Path to the output file.
+        [Parameter()]
+        [string] $Destination = ($Path -replace '\.gz$', '')
+    )
+
+    $fileInput = New-Object System.IO.FileStream $Path, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+    $output = New-Object System.IO.FileStream $Destination, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+    $gzipStream = New-Object System.IO.Compression.GZipStream $fileInput, ([IO.Compression.CompressionMode]::Decompress)
+
+    $buffer = New-Object byte[](1024)
+    while ($true) {
+        $read = $gzipstream.Read($buffer, 0, 1024)
+        if ($read -le 0) { break }
+        $output.Write($buffer, 0, $read)
+    }
+
+    $gzipStream.Close()
+    $output.Close()
+    $fileInput.Close()
+}
+
+
+function Compress-GZipArchive {
+    <#
+        .SYNOPSIS
+        Compresses a file using GZip compression.
+
+        .DESCRIPTION
+        The Compress-GZipArchive function takes a specified file and compresses it using the GZip format.
+        The output file name will be the same as the input file with a `.gz` extension unless a
+        custom destination path is provided.
+
+        .EXAMPLE
+        Compress-GZipArchive -Path "C:\Temp\file.txt"
+
+        Output:
+        ```powershell
+        The file 'C:\Temp\file.txt' is successfully compressed to 'C:\Temp\file.txt.gz'.
+        ```
+
+        Compresses 'C:\Temp\file.txt' into a GZip archive named 'C:\Temp\file.txt.gz'.
+
+        .EXAMPLE
+        Compress-GZipArchive -Path "C:\Temp\file.txt" -Destination "C:\Temp\compressed.gz"
+
+        Output:
+        ```powershell
+        The file 'C:\Temp\file.txt' is successfully compressed to 'C:\Temp\compressed.gz'.
+        ```
+
+        Compresses 'C:\Temp\file.txt' into a GZip archive named 'C:\Temp\compressed.gz'.
+
+        .OUTPUTS
+        System.String. Returns the destination file path after successful compression.
+
+        .LINK
+        https://psmodule.io/Compress/Functions/Compress-GZipArchive
+    #>
+    [CmdletBinding()]
+    param(
+        # Path to the file to be compressed.
+        [Parameter(Mandatory)]
+        [string] $Path,
+
+        # Path to the output compressed file.
+        [Parameter()]
+        [string] $Destination = "$Path.gz"
+    )
+
+    $fileInput = New-Object System.IO.FileStream $Path, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+    $output = New-Object System.IO.FileStream $Destination, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+    $gzipStream = New-Object System.IO.Compression.GZipStream $output, ([IO.Compression.CompressionMode]::Compress)
+
+    $buffer = New-Object byte[](1024)
+    while ($true) {
+        $read = $fileInput.Read($buffer, 0, 1024)
+        if ($read -le 0) { break }
+        $gzipStream.Write($buffer, 0, $read)
+    }
+
+    $gzipStream.Close()
+    $output.Close()
+    $fileInput.Close()
+}
